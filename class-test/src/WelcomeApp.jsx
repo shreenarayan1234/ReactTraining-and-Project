7211e2b193cd81f2d44ad00b75a4a2aafb788fff
shreenarayan1234 <shreenarayanshrestha434@gmail.com>
2024-11-22 15:20:15 +0545

    Remove

--- class-test/src/WelcomeApp.jsx
+++ class-test/src/WelcomeApp.jsx
@@ -1,3 +1,4 @@
+import style from "./components/Welcome.module.css";
 import React from "react";
 import { useState, useEffect } from "react";
 import WelcomeList from "./components/WelcomeList";
@@ -10,35 +11,40 @@ const WelcomeApp = () => {
       let lastName = names[names.length - 1];
       console.log(`Welcome ${lastName}.Hope you have a great day!`);
     }
-  }, [names]);
-
-  useEffect(() => {
     return () => {
       console.log("Unmount");
     };
-  }, []);
+  }, [names]);
+
+
+  const handleRemove = (index) => {
+      setNames((preNames)=>preNames.filter((_,i)=>i !==index)); 
+  };
 
   const handleAdd = () => {
     if (newName.trim() !== "") {
-      setNames([...names, newName]);
+      setNames([newName,...names]);
       setNewName("");
     }
   };
 
   return (
     <>
-      <h2>Welcome List</h2>
-      {names.length < 5 ? (<div>
-        <input
-          type="text"
-          value={newName}
-          onChange={(e) => setNewName(e.target.value)}
-          placeholder="Enter  a name"
-        />
-      <button onClick={handleAdd}>Add Name</button></div>) : (
+      <h2 className={style.welcome_app}>Welcome List</h2>
+      {names.length < 5 ? (
+        <div className={style.input_field}>
+          <input
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            placeholder="Enter  a name"
+          />
+          <button onClick={handleAdd}>Add Name</button>
+        </div>
+      ) : (
         <h3>Too many names</h3>
       )}
-      <WelcomeList names={names} />
+      <WelcomeList names={names} remove={handleRemove} />
     </>
   );
 };
--- class-test/src/components/WelcomeList.jsx
+++ class-test/src/components/WelcomeList.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import style from "./Welcome.module.css";
 
-const WelcomeList = ({ names }) => {
+const WelcomeList = ({ names, remove }) => {
   return (
     <>
-      <h1>List of Name</h1>
+      <h1 className={style.list}>List of Name</h1>
+      <div className={style.namelist}>
       <ul>
         {names.map((name, index) => (
-          <li key={index}>{name}</li>
+          <li key={index} > {name} <button style={{ cursor:"pointer" }} onClick={()=>remove(index)}>x</button></li>
         ))}
       </ul>
+      </div>
+      <h1 className={style.botton}> Total number of Uers {names.length}</h1>
     </>
   );
 };
